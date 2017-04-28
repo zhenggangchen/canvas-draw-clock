187cd9e9e55c6e3b6a6e58dd8400cd384f4d418e
zhenggangchen <458409991@qq.com>
2017-04-28 13:46:33 +0800

    添加无损变换

--- clock.js
+++ clock.js
@@ -6,40 +6,40 @@ var ctx = dom.getContext('2d');
 var width = ctx.canvas.width;
 var height = ctx.canvas.height;
 var r = width / 2;
+var rem = width / 200;
 
 function drawBackground() {
     ctx.save();
     ctx.translate(r, r);
     ctx.beginPath();
-    ctx.arc(0, 0, r - 5, 0, 2 * Math.PI, false);
-    ctx.lineWidth = 10;
+    ctx.lineWidth = 10 * rem;
+    ctx.arc(0, 0, r - ctx.lineWidth / 2, 0, 2 * Math.PI, false);
     ctx.stroke();
 
     var hourNumbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2];
-    ctx.font = '18px Arial';
+    ctx.font = 18 * rem + 'px Arial';
     //让数字对齐的两个方法，两个都要写
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     hourNumbers.forEach(function (number, i) {
         var rad = 2 * Math.PI / 12 * i;
-        var x = Math.cos(rad) * (r - 30);
-        var y = Math.sin(rad) * (r - 30);
+        var x = Math.cos(rad) * (r - 30 * rem);
+        var y = Math.sin(rad) * (r - 30 * rem);
         ctx.fillText(number, x, y);
     });
     for (var i = 0; i < 60; i++) {
         var rad = 2 * Math.PI / 60 * i;
-        var x = Math.cos(rad) * (r - 18);
-        var y = Math.sin(rad) * (r - 18);
+        var x = Math.cos(rad) * (r - 18 * rem);
+        var y = Math.sin(rad) * (r - 18 * rem);
         ctx.beginPath();
         if (i % 5 === 0) {
             ctx.fillStyle = '#000';
-            ctx.arc(x, y, 2, 0, 2 * Math.PI, false);
+            ctx.arc(x, y, 2 * rem, 0, 2 * Math.PI, false);
         } else {
             ctx.fillStyle = '#ccc';
-            ctx.arc(x, y, 2, 0, 2 * Math.PI, false);
+            ctx.arc(x, y, 2 * rem, 0, 2 * Math.PI, false);
 
         }
-
         ctx.fill();
 
     }
@@ -52,9 +52,9 @@ function drawHour(hour, minute) {
     var rad = 2 * Math.PI / 12 * hour;
     var mrad = 2 * Math.PI / 12 / 60 * minute;
     ctx.rotate(rad + mrad);
-    ctx.lineWidth = 6;
+    ctx.lineWidth = 6 * rem;
     ctx.lineCap = 'round';
-    ctx.moveTo(0, 10);
+    ctx.moveTo(0, 10 * rem);
     ctx.lineTo(0, -r / 2);
     ctx.stroke();
     ctx.restore();
@@ -66,10 +66,10 @@ function drawMinute(minute) {
     var rad = 2 * Math.PI / 60 * minute;
     ctx.rotate(rad);
 
-    ctx.lineWidth = 3;
+    ctx.lineWidth = 3 * rem;
     ctx.lineCap = 'round';
-    ctx.moveTo(0, 10);
-    ctx.lineTo(0, -r + 30);
+    ctx.moveTo(0, 10 * rem);
+    ctx.lineTo(0, -r + 30 * rem);
     ctx.stroke();
     ctx.restore();
 }
@@ -79,29 +79,29 @@ function drawSecond(second) {
     ctx.fillStyle = '#c14543';
     var rad = 2 * Math.PI / 60 * second;
     ctx.rotate(rad);
-    ctx.moveTo(-2, 20);
-    ctx.lineTo(2, 20);
-    ctx.lineTo(1, -r + 18);
-    ctx.lineTo(-1, -r + 18);
+    ctx.moveTo(-2 * rem, 20 * rem);
+    ctx.lineTo(2 * rem, 20 * rem);
+    ctx.lineTo(1, -r + 18 * rem);
+    ctx.lineTo(-1, -r + 18 * rem);
     ctx.fill();
     ctx.restore();
 }
 function drawDot() {
     ctx.beginPath();
     ctx.fillStyle = '#fff';
-    ctx.arc(0, 0, 3, 0, 2 * Math.PI, false);
+    ctx.arc(0, 0, 3 * rem, 0, 2 * Math.PI, false);
     ctx.fill();
 }
 
 /*drawBackground();
-drawHour(4, 30);
+ drawHour(4, 30);
  drawMinute(30);
  drawSecond(15);
-drawDot();*/
+ drawDot();*/
 
 //让时针分针秒针动起来
 function draw() {
-    ctx.clearRect(0,0,width,height);
+    ctx.clearRect(0, 0, width, height);
     var now = new Date();
     var hour = now.getHours();
     var minute = now.getMinutes();
@@ -113,8 +113,8 @@ function draw() {
     drawDot();
     ctx.restore();
 }
-
-setInterval(draw, 1000)
+draw();
+setInterval(draw, 1000);
 
 
 
